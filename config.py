037ff080b09c3aa80705bb5fe2b8050ca1468d12
X Roid <97869723+XRoiDX@users.noreply.github.com>
2025-01-12 14:56:27 +0530

    Update config.py

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ import os
 class Config:
     API_ID = os.environ.get("API_ID", "7236453")
     API_HASH = os.environ.get("API_HASH", "33010a70e94f80e55145980072cce969")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7752937328:AAGjzdCMChTFeCMMVHCIUIovhWidBP57n0I") 
     BOT_SESSION = os.environ.get("BOT_SESSION", "forward-bot") 
     DB_URL = os.environ.get("DB_URL", "mongodb+srv://videshi:videshi@videshi.wtffv.mongodb.net/?retryWrites=true&w=majority&appName=videshi")
     DB_NAME = os.environ.get("DB_NAME", "madflixbotz")
